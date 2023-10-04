fb731ba907270e2b0b2cca28019e360229d881cf
KEREM BAS <kerembas2000@gmail.com>
2023-10-04 14:09:13 +0300

    1.0.0

---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  //HTTP CONSTANTS
+  static const String BASE_URL = "https://reqres.in/api/";
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,5 @@
+class Routes {
+  // Service URLs
+  static const API_LOGIN = 'login';
+  static const API_REGISTER = 'register';
+}
